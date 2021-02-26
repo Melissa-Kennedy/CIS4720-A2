4013a061977624c4be246e9132e651e65cae7cd1
alexbmoreira <alexbmoreira@gmail.com>
2021-02-26 17:28:40 -0500

    Sped up stretchG

--- stretchG.py
+++ stretchG.py
@@ -6,13 +6,12 @@ img = cv.imread("testImages/groundTruths/eg1_lowcntrst.jpg", 0)
 # Create zeros array to store the stretched image
 minmax_img = np.zeros((img.shape[0], img.shape[1]), dtype="uint8")
 
-print(img)
-
 # Loop over the image and apply Min-Max formulae
+min_pixel = np.min(img)
+max_pixel = np.max(img)
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
-        minmax_img[i, j] = (img[i, j] - np.min(img)) / (np.max(img) - np.min(img))
-    print(i)
+        minmax_img[i, j] = (img[i, j] - min_pixel) / (max_pixel - min_pixel) * 255
 
 # Displat the stretched image
 cv.imshow("Minmax", minmax_img)
